b3cabf0a00289d2466387dcbf1a915499ecbd0d2
mestihudson <mestihudson@gmail.com>
2014-12-11 13:47:21 -0200

    move method 'amountFor' for 'Rental' class

--- src/main/java/br/com/mestihudson/refactoring/Customer.java
+++ src/main/java/br/com/mestihudson/refactoring/Customer.java
@@ -46,24 +46,7 @@ public class Customer {
     return result;
   }
 
-  private double amountFor(Rental each) {
-    double thisAmount = 0;
-    //determine amounts for each line
-    switch(each.getMovie().getPriceCode()) {
-      case Movie.REGULAR:
-        thisAmount += 2;
-        if(each.getDaysRented() > 2)
-          thisAmount += (each.getDaysRented() - 2) * 1.5;
-        break;
-      case Movie.NEW_RELEASE:
-        thisAmount += each.getDaysRented() * 3;
-        break;
-      case Movie.CHILDRENS:
-        thisAmount += 1.5;
-        if(each.getDaysRented() > 3)
-          thisAmount += (each.getDaysRented() - 3) * 1.5;
-        break;
-    }
-    return thisAmount;
+  private double amountFor(Rental aRental) {
+    return aRental.getCharge();
   }
 }
